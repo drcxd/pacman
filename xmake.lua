66ac72212a619bf58c94d014d7bc77f93276bc65
drcxd <drcxd@sina.com>
2026-09-11 11:05:46 +0800

    feat: SDL example clear

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,79 @@
 add_rules("mode.debug", "mode.release")
 
+package("sdl3-local")
+    add_deps("cmake")
+    set_sourcedir(path.join(os.scriptdir(), "lib", "SDL"))
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs) -- Build and Install with CMake
+    end)
+package_end()
+
+package("sdl3_image-local")
+    add_deps("cmake")
+    add_deps("sdl3-local", {configs = {shared = true}})
+    set_sourcedir(path.join(os.scriptdir(), "lib", "SDL_image"))
+    on_install(function (package)
+        local configs = {}
+        local sdl3 = package:dep("sdl3-local")
+        table.insert(configs, "-DSDL3_DIR=" .. path.join(sdl3:installdir(), "lib", "cmake", "SDL3"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs) -- Build and Install with CMake
+    end)
+package_end()
+
+package("sdl3_mixer-local")
+    add_deps("cmake")
+    add_deps("sdl3-local", {configs = {shared = true}})
+    set_sourcedir(path.join(os.scriptdir(), "lib", "SDL_mixer"))
+    on_install(function (package)
+        local configs = {}
+        local sdl3 = package:dep("sdl3-local")
+        table.insert(configs, "-DSDL3_DIR=" .. path.join(sdl3:installdir(), "lib", "cmake", "SDL3"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs) -- Build and Install with CMake
+    end)
+package_end()
+
+package("sdl3_ttf-local")
+    add_deps("cmake")
+    add_deps("sdl3-local", {configs = {shared = true}})
+    set_sourcedir(path.join(os.scriptdir(), "lib", "SDL_ttf"))
+    on_install(function (package)
+        local configs = {}
+        local sdl3 = package:dep("sdl3-local")
+        table.insert(configs, "-DSDL3_DIR=" .. path.join(sdl3:installdir(), "lib", "cmake", "SDL3"))
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DSDLTTF_VENDORED=ON")
+        import("package.tools.cmake").install(package, configs) -- Build and Install with CMake
+    end)
+package_end()
+
+local packages = {"sdl3-local", "sdl3_image-local", "sdl3_mixer-local", "sdl3_ttf-local"}
+
+for _, pkg in ipairs(packages) do
+   add_requires(pkg, {configs = {shared = true}})
+end
+
 target("pacman")
     set_kind("binary")
     add_files("src/*.cpp")
-
+    add_packages(table.unpack(packages))
+    set_rundir("$(projectdir)")
+    -- Copy all libraries to the directory of the executable
+    after_build(function (target)
+        if target:is_plat("windows", "mingw") then
+            for _, name in ipairs(packages) do
+                local pkg = target:pkg(name)
+                os.cp(path.join(pkg:installdir(), "bin", "*"), target:targetdir())
+            end
+        end
+    end)
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
 --
